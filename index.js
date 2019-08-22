91a79ddfd341d84444d763ce189b30551efc3beb
sijils <sijils@outlook.com>
2019-08-22 12:04:32 +0530

    agn

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.use(express.static(path.join(__dirname,'public')))
 
 routes(app);
 
-app.listen(3000,function(){
-    console.log('Server started on port 3000......')
+app.listen(process.env.PORT ||3000,function(){
+    console.log('Server started .....')
 })
